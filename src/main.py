5fc715a0b2cb58aef26cd391ad746b95c7e33e6f
chenwanheng <chenwanheng@126.com>
2025-06-25 21:36:22 +0800

    初始提交

---
+++ src/main.py
@@ -0,0 +1,7 @@
+# 导入numpy验证docker是否根据requirements文件安装好了依赖
+import os
+
+if __name__ == '__main__':
+    # 在logs目录下创建个名为docker.log的空文件，验证容器和宿主机是否能进行数据同步
+    os.mknod('../logs/docker.log')
+    print("hello docker")
